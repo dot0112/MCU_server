e800e773d49c3013edadc3802346ae48222831e9
dot0112 <dot0112dot0112@gmail.com>
2024-05-19 22:24:47 +0900

    first commit

---
+++ MCU_server/ClientHandler.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using MCU_server;
+
+namespace MCU_server
+{
+	class ClientHandler
+	{
+		private TcpClient client;
+		private NetworkStream stream;
+		private int clientId;
+		private int mode = -1;  // -1:NULL, 0:arduino, 1:client
+		public int ClientId => clientId;
+		byte[] buffer = new byte[1024];
+		int bytesRead;
+
+		public ClientHandler(TcpClient client, int clientId)
+		{
+			this.client = client;
+			this.stream = client.GetStream();
+			this.clientId = clientId;
+		}
+
+		public void HandleClient()
+		{
+			try
+			{
+				while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+				{
+					string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+					if (data == "arduino")
+					{
+						lock (main.syncLock)
+						{
+							if (main.arduino == -1)
+							{
+								main.arduino = clientId;
+								main.SendCommandToClient(clientId, "Arduino");
+								mode = 0;
+							}
+							else
+							{
+								main.SendCommandToClient(clientId, "fail");
+								continue;
+							}
+						}
+					}
+					if (data == "client")
+					{
+						lock (main.syncLock)
+						{
+							if (main.client == -1)
+							{
+								main.client = clientId;
+								main.SendCommandToClient(clientId, "Client");
+								mode = 1;
+							}
+							else
+							{
+								main.SendCommandToClient(clientId, "fail");
+								continue;
+							}
+						}
+					}
+					break;
+				}
+
+				if(mode == 0)
+				{
+					socket_arduino();
+				} 
+				if(mode == 1)
+				{
+					socket_client();
+				}
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"클라이언트 [{clientId}] 처리 중 예외 발생: {e.Message}");
+			}
+			finally
+			{
+				// 클라이언트 연결 종료
+				if (stream != null)
+					stream.Close();
+				if (client != null)
+					client.Close();
+				Console.WriteLine($"클라이언트 [{clientId}] 연결 종료");
+				lock (main.syncLock)
+				{
+					if (clientId == main.arduino) main.arduino = -1;
+					if (clientId == main.client) main.client = -1;
+				}
+			}
+		}
+
+		public void SendMessage(string message)
+		{
+			if (client.Connected)
+			{
+				byte[] msg = Encoding.ASCII.GetBytes(message);
+				stream.Write(msg, 0, msg.Length);
+			}
+		}
+
+		public void socket_arduino()
+		{
+			while (true)
+			{
+				while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+				{
+					DateTime startTime = DateTime.Now; // 선언 위치 확인 필요
+					while ((DateTime.Now - startTime).TotalSeconds < 3) {}
+					// 재요청 전달
+					main.SendCommandToClient(main.arduino, "r");
+				}
+			}
+		}
+
+		public void socket_client()
+		{
+			while (true)
+			{
+				while((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+				{
+					string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+					if(data=="w")
+					{
+						main.SendCommandToClient(main.arduino, "w");
+					}
+					if (data == "s")
+					{
+						main.SendCommandToClient(main.arduino, "s");
+					}
+					if(data == "a")
+					{
+						main.SendCommandToClient(main.arduino, "a");
+					}
+					if (data == "d")
+					{
+						main.SendCommandToClient(main.arduino, "d");
+					}
+					main.SendCommandToClient(clientId, " ");
+				}
+			}
+		}
+	}
+}
---
+++ MCU_server/main.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace MCU_server
+{
+	internal class main
+	{
+		private static TcpListener server = null;
+		private static int clientCounter = 0; // 클라이언트 ID를 위한 카운터
+		public static int arduino = -1, client = -1;
+		public static object syncLock = new object(); // 동기화 객체
+
+		private static List<ClientHandler> clientHandlers = new List<ClientHandler>(); // 클라이언트 핸들러 리스트
+		static void Main(string[] args)
+		{
+			try
+			{
+				IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+				int port = 12345;
+				server = new TcpListener(localAddr, port);
+
+				server.Start();
+				Console.WriteLine($"서버가 {localAddr}:{port}에서 대기 중입니다...");
+
+				while (true)
+				{
+					TcpClient client = server.AcceptTcpClient();
+					int clientId = ++clientCounter; // 클라이언트 ID 할당
+					Console.WriteLine($"클라이언트 [{clientId}]가 연결되었습니다!");
+
+					// 클라이언트 처리를 위한 ClientHandler 인스턴스 생성
+					ClientHandler clientHandler = new ClientHandler(client, clientId);
+
+					lock (syncLock)
+					{
+						clientHandlers.Add(clientHandler);
+					}
+
+					// 클라이언트 처리를 위한 스레드 생성
+					Thread clientThread = new Thread(new ThreadStart(clientHandler.HandleClient));
+					clientThread.Start();
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("예외: " + e.Message);
+			}
+			finally
+			{
+				if (server != null)
+					server.Stop();
+			}
+		}
+		public static void RemoveClientHandler(int clientId)
+		{
+			lock (syncLock)
+			{
+				clientHandlers.RemoveAll(ch => ch.ClientId == clientId);
+			}
+		}
+
+		public static void SendCommandToClient(int clientId, string message)
+		{
+			lock (syncLock)
+			{
+				ClientHandler handler = clientHandlers.Find(ch => ch.ClientId == clientId);
+				if (handler != null)
+				{
+					handler.SendMessage(message);
+				}
+				else
+				{
+					Console.WriteLine($"클라이언트 [{clientId}]를 찾을 수 없습니다.");
+					return;
+				}
+			}
+		}
+	}
+}
